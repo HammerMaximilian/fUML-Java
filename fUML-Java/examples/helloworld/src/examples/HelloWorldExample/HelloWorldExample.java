3ab9e308951a96c163fbe6546dbbafccbe16ea78
Maximilian Hammer <58948559+HammerMaximilian@users.noreply.github.com>
2024-07-01 17:58:38 +0200

    Added HelloWorldExample

---
+++ fUML-Java/examples/helloworld/src/examples/HelloWorldExample/HelloWorldExample.java
@@ -0,0 +1,9 @@
+package examples.HelloWorldExample;
+
+public class HelloWorldExample {
+
+	public static void main(String[] args) {
+		HelloWorldExampleEnvironment.instance().execute("main");
+	}
+
+}
