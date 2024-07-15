4bc0a6975b420f1481cc207a3e81ebb748550597
Maximilian Hammer <58948559+HammerMaximilian@users.noreply.github.com>
2024-07-15 16:52:19 +0200

    Added model as well as source code for fuml-tests

---
+++ fUML-Java/tests/fuml-tests/src/fUML_Tests/fUML_Tests.java
@@ -0,0 +1,28 @@
+package fUML_Tests;
+
+public class fUML_Tests
+{
+	public static void printUsage()
+	{
+		System.out.println(
+            "=============================================================================================\n" +
+            "USAGE: <program-name> <behavior-name> [<behavior-name> <behavior-name> <behavior-name> <...>]\n" +
+            "=============================================================================================\n"
+            );
+	}
+
+	public static void main(String[] args)
+	{
+		if(args.length == 0)
+		{
+			System.out.println("[ERROR] Must declare at least one behavior name.\n");
+			printUsage();
+			return;
+		}
+	
+		for (String arg : args)
+		{
+			fUML_TestsEnvironment.instance().execute(arg);
+		}
+	}
+}
